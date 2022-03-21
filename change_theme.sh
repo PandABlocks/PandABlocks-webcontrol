f5c3114bdf32d3b3688f2cd3e7be7f5ca1f6a089
Tom Trafford <tom.trafford@diamond.ac.uk>
2022-03-21 14:15:55 +0000

    Correct theme for docs

--- change_theme.sh
+++ change_theme.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
+
+# Remove multiple lines to be changed. They are then replaced within the following command
+sed -e "/if not on_rtd:.*/{n;N;N;N;N;N;d}" -i $1
+
 sed -e "s|html_logo = .*|html_logo = 'PandA-logo-for-black-background.svg'|" \
     -e "s|html_favicon = .*|html_favicon = 'favicon.ico'|" \
     -e "s|project = .*|project = 'PandABlocks-webcontrol'|" \
+    -e "s|if not on_rtd:.*|try:\n    import sphinx_rtd_theme\n    html_theme = 'sphinx_rtd_theme'\nexcept ImportError:\n    html_theme = 'default'\n    print('sphinx_rtd_theme not found, using default')|"\
     -i $1
 
 cat << EOF > $2
