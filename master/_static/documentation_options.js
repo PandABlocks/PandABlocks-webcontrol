3741d609ab9a89347a08bb2cc7a9f180378957e4
tomtrafford <tomtrafford@users.noreply.github.com>
2023-09-15 13:12:32 +0000

    deploy: f9a90244fc97f74fd8f6b0ef422a447019278d96

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.1-6-gf663e6d',
+    VERSION: '2.1-6-gf9a9024',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
