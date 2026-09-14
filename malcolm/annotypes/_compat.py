3057d072e919e1931c31b7397531a3742937fc88
Emilio Perez Juarez <emiliopeju@gmail.com>
2026-09-14 16:19:53 +0100

    annotypes: fix compat for newer python

--- malcolm/annotypes/_compat.py
+++ malcolm/annotypes/_compat.py
@@ -1,5 +1,4 @@
 import inspect
-import sys
 
 
 # Taken from six
@@ -14,24 +13,11 @@ def add_metaclass(metaclass):
 
 
 def getargspec(f):
-    if sys.version_info < (3,):
-        args, varargs, keywords, defaults = inspect.getargspec(f)
-    else:
-        # Need to use fullargspec in case there are annotations
-        args, varargs, keywords, defaults = inspect.getfullargspec(f)[:4]
-    return inspect.ArgSpec(args, varargs, keywords, defaults)
+    return inspect.getfullargspec(f)
 
 
 def func_globals(f):
-    if sys.version_info < (3,):
-        return f.func_globals
-    else:
-        return f.__globals__
+    return f.__globals__
 
 
-if sys.version_info < (3,):
-    # python 2
-    str_ = basestring
-else:
-    # python 3
-    str_ = str
+str_ = str
