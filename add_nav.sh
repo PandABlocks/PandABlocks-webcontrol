091f09d0a595c533b02bb15c740693cdbead1587
Emilio Perez Juarez <emiliopeju@gmail.com>
2026-09-22 11:30:35 +0100

    Remove deprecated files and correct metadata

--- add_nav.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-template='{% raw admin_loader.load("nav.html").generate(active="panda-webcontrol", etc_loader=etc_loader, request=request) %}'
-
-sed -e "s|</body>|$template</body>|" $1
