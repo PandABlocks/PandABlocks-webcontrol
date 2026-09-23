Fix masking overflow problem when field is int32 and mask is 0xffffffff
numpy was implicitly converting the mask value to int32 which overflows,
this was fixed by representing the values as uint32.

malcolm/modules/pandablocks/parts/pandatablepart.py
@@ -117,7 +117,8 @@ class PandATablePart(PandAFieldPart):
                 column_value = column_value.seq
             # Left shift the value so it is aligned with the int columns
             _, mask = get_nbits_mask(field_data)
-            shifted_column = (column_value & mask) << field_data.bits_lo % 32
+            shifted_column = (column_value.astype(np.uint32) & np.uint32(mask)) \
+                << (field_data.bits_lo % 32)
             # Or it with what we currently have
             column_index = get_column_index(field_data)
             int_matrix[..., column_index] |= shifted_column.astype(np.uint32)